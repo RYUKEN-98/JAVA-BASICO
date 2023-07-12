676ca4f58670cc7cbd260cedd81b37701359aca6
DIEGO MAZO <damazo1981@gmail.com>
2023-07-11 21:24:44 -0500

    11/07/23

--- js/hola-mundo.js
+++ js/hola-mundo.js
@@ -1,1 +1,9 @@
-//crear la app de hola mundo/*
+// crear la app de Hola Mundo
+/**
+ * @returns {String} hola mundo es una cadena de texto
+ * 
+ */ 
+
+// imprime en la consola del navegador
+console.log('Hola Mundo'); 
+console.warn('Hola Mundo'); 
